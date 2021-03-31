e9e14e26b487b25667b19d99057224feaaeb48ef
Christina Pagan <enterprise@christinas-mbp.home>
2021-03-31 12:38:31 -0400

    user searches within form and console prints city weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,25 @@ var getWeather = function(userCity) {
     })
 }
 
-getWeather('Orlando');
+var userForm = document.querySelector('#userForm');
+var userCity = document.querySelector('#userCity');
+var btnClick = document.querySelector('#btnClick');
+
+
+
+//listen to when the user clicks the search button
+var formSubmitHandler = function(event) 
+{
+    event.preventDefault();
+
+var userInput = userCity.value.trim();
+
+if(userInput) {
+    getWeather(userInput);
+}
+else{
+    alert('Please enter a city');
+}
+}
+
+btnClick.addEventListener('click', formSubmitHandler);
