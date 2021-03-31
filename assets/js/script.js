c3abccb66f2cea82c443f04b19d35c18e692c589
Christina Pagan <enterprise@christinas-mbp.home>
2021-03-31 11:46:41 -0400

    added user parameter to API search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
-var getApi = function() {
-    fetch("http://api.openweathermap.org/data/2.5/weather?q=orlando&APPID=b7f5bbcf25f5227f04a67e383665ed91").then(function(response){
+var getWeather = function(userCity) {
+    var apiUrl = ("http://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&APPID=b7f5bbcf25f5227f04a67e383665ed91");
+
+    fetch(apiUrl).then(function(response){
         response.json().then(function(data){
             console.log(data);
         })
     })
 }
 
-getApi();
+getWeather('Orlando');
