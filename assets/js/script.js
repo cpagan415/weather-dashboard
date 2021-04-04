502385a51eaba5fe6b264f3494fbc5d3f1f4f6c5
Christina Pagan <enterprise@christinas-mbp.home>
2021-04-04 00:22:51 -0400

    icon displays correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,16 @@
 var getWeather = function(userCity) {
     var apiUrl = ('https://api.openweathermap.org/data/2.5/weather?q=' + userCity + '&appid=b7f5bbcf25f5227f04a67e383665ed91');
     fetch(apiUrl).then(function(response){
+        if(response.ok){
         response.json().then(function(data){
             displayWeather(data,userCity);
-            console.log(data);})})}
+            console.log(data);
+            });
+        }else {
+            alert(response.statusText);
+        }
+        })
+    }
 
 var fiveDayForecast = function(lat,lon)
 {
@@ -48,6 +55,7 @@ var formSubmitHandler = function()
     histSearchBtn(locCity);
     getWeather(locCity);
     clearForm();
+    
 }
 var clearForm = function()
 {
@@ -75,18 +83,27 @@ var storeData = function()
 }
 
 //displaying weather api
-function displayWeather(weather,locCity){
+function displayWeather(weatherInfo,locCity){
 
     var weatherDis = document.getElementById('weatherDis');
    //Api Info
-    var tempF = 'Temp: ' + (Math.round(weather.main.temp - 273)*(9/5)+ 32) + '˚F<br>';
-    var locDate = moment().format('MMMM Do YYYY') +'<br>';
+    var tempF = 'Temp: ' + (Math.round(weatherInfo.main.temp - 273)*(9/5)+ 32) + '˚F<br>';
+    var locDate = moment().format('MMMM Do YYYY');
     locCity = locCity.slice(0,1).toUpperCase() + locCity.substring(1, locCity.length);
-    var lon = weather.coord.lon;
-    var lat = weather.coord.lat;
-    var wind = 'Wind Spd:' + weather.wind.speed + 'mph<br>';
-    var humidity = 'Humidity: ' + weather.main.humidity + '%<br>';
-    weatherDis.innerHTML = locCity+ ' ' + locDate + tempF + wind + humidity;
+  //title with icon display
+  var titleIcon = document.getElementById('titleIcon');
+  var img = document.createElement('img');
+  img.setAttribute('src', 'http://openweathermap.org/img/wn/' + weatherInfo.weather[0].icon + '.png');
+  
+    var lon = weatherInfo.coord.lon;
+    var lat = weatherInfo.coord.lat;
+    var wind = 'Wind Spd:' + weatherInfo.wind.speed + 'mph<br>';
+    var humidity = 'Humidity: ' + weatherInfo.main.humidity + '%<br>';
+    //Uv Index: error with displaying uv index 
+    titleIcon.textContent = locCity + ' ' + locDate;
+    titleIcon.append(img);
+    weatherDis.innerHTML = tempF + wind + humidity;
+  
     
     fiveDayForecast(lat,lon);
 }
@@ -106,14 +123,22 @@ var fiveDay = function(info)
        var tempF = 'Temp: ' + Math.round((temp -273) * (9/5) + 32) + '˚F<br>';
        var wind = 'WindSpd: ' + info.daily[i].wind_speed + ' mph<br>';
        var humidity= 'Humidity: '+ info.daily[i].humidity +'%<br>';
+
+       //icon not displaying for 5 day weather
+       /*
+       var wIcon = document.getElementById('wIcon');
+        var img2 = document.createElement('img2');
+        img2.setAttribute('src', 'http://openweathermap.org/img/wn/' + info.daily[i].weather[0].icon + '.png');
+        */
        create.setAttribute('class', 'fiveDay border-dark cards m-2');
-       create.setAttribute('style', 'width: 12rem')
+       create.setAttribute('style', 'width: 12rem');
+      
        create.innerHTML = dates + tempF + wind+ humidity;
        displayFive.appendChild(create);
        
    }  
  }
- //var convUnix = moment.unix(unixTime).format('MMMM Do YYYY');
+ 
 
  displayFiveDay();
 
